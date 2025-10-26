520cfcc492d5051675ce129afacc64bbea63f3a3
Leekuejea <starLee2427@gmail.com>
2025-10-26 18:34:37 +0900

    알림it_1.0.1 [오류 수정: 1. 초기화문제 2. 알림전공 수정 3. 알림 상태 전달]

--- lib/keys.dart
+++ lib/keys.dart
@@ -1,4 +1,4 @@
-const version = '1.0.0';
+const version = '1.0.1';
 const KHasSeenIntro = 'hasSeenIntro';
 const kConsentKey = 'privacy_consent_v1';
 const String port = 'https://alarm-it.ulsan.ac.kr';
