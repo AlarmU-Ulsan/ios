0c132184a8c5be8fb7611d46cf694be0ab56b36a
Leekuejea <starLee2427@gmail.com>
2026-01-04 17:35:12 +0900

    알림it_1.0.3 [오류 수정: 초기화문제 해결]

--- lib/keys.dart
+++ lib/keys.dart
@@ -1,7 +1,7 @@
 const version = '1.0.1';
 const KHasSeenIntro = 'hasSeenIntro';
 const kConsentKey = 'privacy_consent_v1';
-const String port = 'https://alarm-it.ulsan.ac.kr/test';
+const String port = 'https://alarm-it.ulsan.ac.kr';
 const kAlarmListKey = 'alram_list';       // 선택한 학부/전공 리스트
 const kIsAllAlarmOnKey = 'isAllAlarmOn';  // 전체 알림 스위치 상태
 const String kMainMajorKey = 'main_major';        // Page1: 대표 전공(1개)
